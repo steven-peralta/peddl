346168ca9cc7e30a43105da36c517dec34ea616a
Steven Peralta <k9rosie@gmail.com>
2022-09-18 14:41:18 -0500

    add axios and remove cors from dependencies

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import APP_NAME from '@peddl/common';
+import cors from 'cors';
 
 const app = express();
 const port = 8080;
+
+app.use(cors());
 
 app.get('/', (_test, res) => {
   res.send(APP_NAME);
