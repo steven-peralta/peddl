06ed9e24611eac9fdbf825892c98f4c89867f661
Jbennett1498 <102630118+Jbennett1498@users.noreply.github.com>
2022-10-06 13:19:31 -0500

    created a EditSearchSettingForm.tsx (this will need to be edited to makes sure that the layout it working correctly) and also edited the SearchSettingsStep.tsx (this will also have to be editted). These changes are a rough draft that I did during class.

---
+++ packages/ui/src/components/forms/EditSearchSettingsForm/EditSearchSettingForm.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+import { TagsInput } from 'react-tag-input-component';
+
+export type EditSearchSettingFormProps = {
+  onRangeSettingChange: () => void;
+  onGenderSettingChange: (gendersSetting: string[]) => void;
+  onLocationSettingChange: (locationsSetting: string[]) => void;
+  onGenreSettingInputChange: (genresSetting: string[]) => void;
+  onTalentSettingInputChange: (talentsSetting: string[]) => void;
+};
+
+export default function EditSearchSettingForm({
+  onRangeSettingChange,
+  onGenderSettingChange,
+  onLocationSettingChange,
+  onGenreSettingInputChange,
+  onTalentSettingInputChange,
+}: EditSearchSettingFormProps) {
+  return (
+    <div>
+      <Form.Group>
+        <Form.Range
+          getAriaLabel={() => 'Minimum distance'}
+          value={value1}
+          onChange={onRangeSettingChange}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          disableSwap
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Gender</Form.Label>
+        <div>
+          <TagsInput
+            onChange={onGenderSettingChange}
+            name="gendersSetting"
+            placeHolder="Genders"
+          />
+          <em>Press enter to add new tag</em>
+        </div>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Locations</Form.Label>
+        <div>
+          <TagsInput
+            onChange={onLocationSettingChange}
+            name="locationsSetting"
+            placeHolder="Locations"
+          />
+          <em>Press enter to add new tag</em>
+        </div>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Genres</Form.Label>
+        <div>
+          <TagsInput
+            onChange={onGenreSettingInputChange}
+            name="genresSetting"
+            placeHolder="Genres"
+          />
+          <em>Press enter to add new tag</em>
+        </div>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Talents</Form.Label>
+        <div>
+          <TagsInput
+            onChange={onTalentSettingInputChange}
+            name="talentsSetting"
+            placeHolder="Talents"
+          />
+          <em>Press enter to add new tag</em>
+        </div>
+      </Form.Group>
+    </div>
+  );
+}
--- packages/ui/src/content/CreateAccount/SearchSettingsStep.tsx
+++ packages/ui/src/content/CreateAccount/SearchSettingsStep.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import EditSearchSettingForm from '../../components/forms/EditSearchSettingsForm/EditSearchSettingForm';
 
 export default function SearchSettingsStep() {
-  return <p>search settings</p>;
+  const [gendersSetting, setGenderSetting] = useState<string[]>([]);
+  const [locationsSetting, setLocationSetting] = useState<string[]>([]);
+  const [genresSetting, setGenreSetting] = useState<string[]>([]);
+  const [talentsSetting, setTalentSetting] = useState<string[]>([]);
+  return (
+    <EditSearchSettingForm
+      onRangeSettingChange={}
+      onGenderSettingChange={setGenderSetting}
+      onLocationSettingChange={setLocationSetting}
+      onGenreSettingInputChange={setGenreSetting}
+      onTalentSettingInputChange={setTalentSetting}
+    />
+  );
 }
