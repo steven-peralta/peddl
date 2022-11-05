c68bdc9d05cdd89f3d09300fa89eb6f5df0c08bc
Steven Peralta <k9rosie@gmail.com>
2022-11-05 18:23:19 -0500

    change path (again)

--- packages/ui/src/providers/WebsocketProvider.tsx
+++ packages/ui/src/providers/WebsocketProvider.tsx
@@ -3,13 +3,21 @@ import { io, Socket } from 'socket.io-client';
 
 export const baseURL =
   process.env['REACT_APP_ENVIRONMENT'] === 'staging'
-    ? 'http://api-staging.peddl.chat/wss'
+    ? 'http://api-staging.peddl.chat/'
     : process.env['REACT_APP_ENVIRONMENT'] === 'production' ||
       process.env.NODE_ENV === 'production'
-    ? 'https://api.peddl.chat/wss'
+    ? 'https://api.peddl.chat/'
     : 'http://localhost:8000/';
 
-const socket = io(baseURL);
+export const path =
+  process.env['REACT_APP_ENVIRONMENT'] === 'staging'
+    ? '/wss'
+    : process.env['REACT_APP_ENVIRONMENT'] === 'production' ||
+      process.env.NODE_ENV === 'production'
+    ? '/wss'
+    : '/';
+
+const socket = io(baseURL, { path });
 
 export const WebsocketContext = createContext<Socket>(socket);
 
